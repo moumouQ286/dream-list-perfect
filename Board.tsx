c9df11af5547a38d4b7faa9385dc63aa87dda161
moumouQ286 <moumouQ@hotmail.com>
2026-05-22 16:15:42 +0800

    Update Board.tsx

--- Board.tsx
+++ Board.tsx
@@ -3,7 +3,7 @@ import { Plus, Layout, GripVertical } from "lucide-react";
 import { motion, AnimatePresence, Reorder } from "motion/react";
 import { StickyNote } from "./StickyNote";
 import { StickyNoteData, AgeTab, AGE_TABS, STICKY_NOTE_COLORS } from "../types";
-import { ... } from "./utils";
+import { cn } from "./utils";
 
 interface BoardProps {
   userAge: number;
