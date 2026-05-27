02d3db8478b02e28bb644ffa47d09c6f1d24f997
moumouQ286 <moumouQ@hotmail.com>
2026-05-27 13:24:58 +0800

    Update Board.tsx

--- Board.tsx
+++ Board.tsx
@@ -255,7 +255,7 @@ export const Board: React.FC<BoardProps> = ({
           </button>
           <div className="flex items-center gap-2">
             <div className="w-2 h-2 rounded-full bg-green-500 shadow-[0_0_8px_rgba(34,197,94,0.5)]"></div>
-            <span className="uppercase tracking-widest">TEST 123</span>
+            <span className="uppercase tracking-widest">Local Storage Active / 已本地儲存</span>
           </div>
         </div>
       </footer>
